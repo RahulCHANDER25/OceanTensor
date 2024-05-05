35a4de88c705169cfc5e2a6f77087fa04467f0f0
rahul.chander <rahul.chander@epitech.eu>
2024-05-05 15:14:54 +0200

    fix: Const on print functions and indent in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,24 @@
  
 int main()
 {
-   OceanTensor::myTensor<int, 3> tensor({3, 3, 3});
-   OceanTensor::myTensor<int, 3> tensor2({3, 3, 3});
+    OceanTensor::myTensor<int, 3> tensor({3, 3, 3});
+    OceanTensor::myTensor<int, 3> tensor2({3, 3, 3});
 
-   OceanTensor::myTensor<int, 3> res1 = tensor + tensor2;
-   OceanTensor::myTensor<int, 3> res2 = tensor - tensor2;
-   OceanTensor::myTensor<int, 3> res3 = tensor * tensor2;
+    OceanTensor::myTensor<int, 3> res1 = tensor + tensor2;
+    OceanTensor::myTensor<int, 3> res2 = tensor - tensor2;
+    OceanTensor::myTensor<int, 3> res3 = tensor * tensor2;
 
-   std::cout << "-- LEFT --" << std::endl;
-   tensor.dump_arr();
-   std::cout << "-- RIGHT --" << std::endl;
-   tensor2.dump_arr();
+    std::cout << "-- LEFT --" << std::endl;
+    tensor.dump_arr();
+    std::cout << "-- RIGHT --" << std::endl;
+    tensor2.dump_arr();
 
-   std::cout << "---- ADD ----" << std::endl;
-   res1.dump_arr();
-   std::cout << "---- MINUS ----" << std::endl;
-   res2.dump_arr();
-   std::cout << "---- MULTIPLY ----" << std::endl;
-   res3.dump_arr();
-   std::cout << "--------------------------------" << std::endl;
+    std::cout << "---- ADD ----" << std::endl;
+    res1.dump_arr();
+    std::cout << "---- MINUS ----" << std::endl;
+    res2.dump_arr();
+    std::cout << "---- MULTIPLY ----" << std::endl;
+    res3.dump_arr();
+    std::cout << "--------------------------------" << std::endl;
     return 0;
 }
-
